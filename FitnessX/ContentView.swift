6b567a33116cb464323d66dd8959a236d82ff91b
Dinil Jayatunge <diniljayatu@gmail.com>
2026-01-25 19:21:04 +0000

    Initial Commit

---
+++ FitnessX/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FitnessX
+//
+//  Created by Dinil Jayatunge on 2026-01-25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FitnessX/FitnessXApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitnessXApp.swift
+//  FitnessX
+//
+//  Created by Dinil Jayatunge on 2026-01-25.
+//
+
+import SwiftUI
+
+@main
+struct FitnessXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
